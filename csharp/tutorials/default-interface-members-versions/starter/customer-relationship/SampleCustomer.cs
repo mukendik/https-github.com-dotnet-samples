3db2e8e264ce99d04805425bd617fc090e562cff
MUKENDI KALONJI GHISLAIN <mukendik@gmail.com>
2019-11-19 09:02:04 +0100

    Set Loyalty and computeLoyalty

--- csharp/tutorials/default-interface-members-versions/starter/customer-relationship/SampleCustomer.cs
+++ csharp/tutorials/default-interface-members-versions/starter/customer-relationship/SampleCustomer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace customer_relationship
 {
@@ -27,5 +28,27 @@ namespace customer_relationship
                 LastOrder = order.Purchased;
             allOrders.Add(order);
         }
+
+        // Version 2: 
+        public static void SetLoyaltyThresholds(
+            TimeSpan ago, 
+            int minimumOrders = 10, 
+            decimal percentageDiscount = 0.10m) 
+        {   
+            length = ago;
+            orderCount = minimumOrders;
+            discountPercent = percentageDiscount; 
+        } 
+        private static TimeSpan length = new TimeSpan(365 * 2, 0,0,0); // two years 
+        private static int orderCount = 10; 
+        private static decimal discountPercent = 0.10m;
+        public decimal ComputeLoyaltyDiscount()
+        {
+            DateTime start = DateTime.Now - length;
+            if ((DateJoined < start) && (PreviousOrders.Count() > orderCount))
+            { return discountPercent; }
+            return 0;
+        }
+
     }
 }
